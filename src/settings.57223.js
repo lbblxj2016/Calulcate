5be23a2189c64f3c069513a6b871e215d1ba4bf5
lbblxj2016 <65338628+lbblxj2016@users.noreply.github.com>
2024-04-05 15:31:59 +0800

    Delete src directory

--- src/settings.57223.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","wall","hero"],collisionMatrix:[[true],[false,false,true],[false,true,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",resources:"b15c0",main:"beae0"}};
